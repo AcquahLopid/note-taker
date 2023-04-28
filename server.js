8769d4d59f5af4a7c86402690601506dc67f452e
Acquah <emanuel.molina2k@gmail.com>
2023-04-27 19:17:18 -0700

    add api json request

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const NotesDb = require('./Develop/db/db.json'); 
 
 const PORT = 3001;
 
@@ -19,8 +20,17 @@ app.get('/', (req, res) =>
 
 // get Route for notes created
 app.get('/notes', (req, res) => 
-    res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
+res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
 );
+
+//
+app.get('/api', (req, res) => res.json(NotesDb));
+
+// POST request
+app.post('/api/notes', (req, res) => {
+  // Let the client know that their POST request was received
+  res.json(`${req.method} request received`);
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
