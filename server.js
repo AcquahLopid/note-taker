cd19227ec568ad51d9bc9fb368d39deaa248e68e
Acquah <emanuel.molina2k@gmail.com>
2023-04-27 20:03:27 -0700

    add readfile for api/notes

--- server.js
+++ server.js
@@ -2,35 +2,62 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const NotesDb = require('./Develop/db/db.json'); 
+const { readAndAppend, readFromFile } = require('./helpers/fsUtils')
 
 const PORT = 3001;
-
 const app = express();
+
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-
 // get Route for homepage
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
+  res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
 );
 
 // get Route for notes created
 app.get('/notes', (req, res) => 
-res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
+  res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
 );
 
-//
-app.get('/api', (req, res) => res.json(NotesDb));
-
 // POST request
-app.post('/api/notes', (req, res) => {
-  // Let the client know that their POST request was received
-  res.json(`${req.method} request received`);
+app.get('/api/notes', (req, res) => {
+
+  fs.readFile(NotesDb, (err, data) => {
+      if(err){
+        console.error(err);
+      } else if (!err){
+        // parses data if it isnt an error
+        const parsedJson = JSON.parse(data);
+        return res.json(parsedJson);
+      } else{
+        // returns empty array 
+        return res.json([]);
+      }
+    });
 });
+
+app.post('/api/notes', (req, res) => {
+  
+})
+
+app.post('/api', (req, res) => res.json(NotesDb));
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
